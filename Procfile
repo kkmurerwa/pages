f88a0ea09d252da95de9ee238ba5dc56ed5c9825
xwaxes01 <xwaxes@gmail.com>
2020-07-09 20:07:26 +0300

    Files staged for Heroku deployment

--- Procfile
+++ Procfile
@@ -1,12 +1,1 @@
-[[source]]
-name = "pypi"
-url = "https://pypi.org/simple"
-verify_ssl = true
-
-[dev-packages]
-
-[packages]
-django = "~=3.0.0"
-
-[requires]
-python_version = "3.8"
+web: gunicorn mb_project.wsgi --log-file -
--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0$*lzy$^szlfi!&jhbqg*n3#-ieqpp1qkojy0)sw^d&*w_hno+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
